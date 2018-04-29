6f76120b634434ae0d4df28ec962b7d515b9357d
Dan Stough <Daniel.Stough@gmail.com>
2018-04-29 18:51:13 -0400

    adds apache recipe to recipes/default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,16 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+#
+# This is my first attempt at using vim
+# This is my second attempt at using vim
+
+package 'httpd'
+
+file '/var/www/html/index.html' do
+  content '<h1>Hello, world!</h1>'
+end
+
+service 'httpd' do
+  action [:enable, :start]
+end
